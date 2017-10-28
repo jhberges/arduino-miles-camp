eeeef7082dcfe2f960b820de7fdf11e10e2c1899
Jan-Helge Bergesen <jhberges@gmail.com>
2017-10-28 15:39:16 +0200

    Finishing up

--- sketch_oct28d/sketch_oct28d.ino
+++ sketch_oct28d/sketch_oct28d.ino
@@ -22,8 +22,7 @@ void setup() {
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid);//, pass);
+    status = WiFi.begin(ssid);
 
     // wait 10 seconds for connection:
     delay(10000);
---
+++ sketch_oct28e/sketch_oct28e.ino
@@ -0,0 +1,21 @@
+#include <BLETypedCharacteristics.h>
+#include <BLEService.h>
+#include <CurieBLE.h>
+#include <BLECharacteristic.h>
+#include <BLEPeripheral.h>
+#include <BLEDevice.h>
+#include <BLETypedCharacteristic.h>
+#include <BLEDescriptor.h>
+#include <BLECentral.h>
+#include <BLEAttributeWithValue.h>
+#include <BLECommon.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
